facd8d9146e8d655feac7b2cb623c72dcc445858
Mrbabet <wieliczkokamil@gmail.com>
2022-12-16 20:41:31 +0100

    Added toggling mobile nav

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuAcitve = document.querySelector(".burger-button");
+const mobileNav = document.querySelector(".nav-mobile");
+
+let buttonActive = false;
+
+menuAcitve.addEventListener("click", () => {
+  if (!buttonActive) {
+    mobileNav.classList.add("active");
+    menuAcitve.classList.add("active");
+    // faMoon.classList.add("active");
+    // icon.classList.add("active");
+    buttonActive = true;
+  } else {
+    mobileNav.classList.remove("active");
+    menuAcitve.classList.remove("active");
+    // faSun.classList.remove("active");
+    // faMoon.classList.remove("active");
+    // icon.classList.remove("active");
+    buttonActive = false;
+  }
+});
